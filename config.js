2dcb0828695d23a3804a3fc16ccf2fb349d48c7e
LORD-Zenix <nandananda2235@gmail.com>
2022-04-10 13:15:16 +0700

    Loli-MD

---
+++ config.js
@@ -0,0 +1,60 @@
+/**
+* Thanks To
+* Allah S.W.T
+* Ortu
+* BOT INDO
+* Dika Ardnt
+* Sanzy Kawaii
+* FIRAA
+* Guntur
+* Dhika Ardiant
+* All Creator Bot
+* All Subscriber Ku
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['6285701863281'] //ganti jadi nomor lu
+global.premium = ['6285701863281'] //nomornya samain kaya yg di atas
+global.packname = 'ZennBot MD' //ganti aja sesuai keinginan lu
+global.author = 'Zenix MD' //ganti aja
+global.sessionName = 'lolibot' //ini buat nama session terserah mau ganti apa nggk
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    eror: 'Yah Eror kak',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/apri.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
